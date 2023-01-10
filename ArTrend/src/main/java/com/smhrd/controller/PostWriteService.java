145f5b0253bf596b67a79cc79b97eb9dac0ca1ad
SMHRD <SMHRD@218.157.38.84>
2023-01-10 19:47:12 +0900

    PostWriteService - 로그인 한 email 추출 추가 - 현아

--- ArTrend/src/main/java/com/smhrd/controller/PostWriteService.java
+++ ArTrend/src/main/java/com/smhrd/controller/PostWriteService.java
@@ -8,11 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.smhrd.model.BoardsDAO;
 import com.smhrd.model.BoardsVO;
+import com.smhrd.model.UserVO;
 
 public class PostWriteService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -21,6 +23,10 @@ public class PostWriteService extends HttpServlet {
 			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		UserVO info_post = (UserVO)session.getAttribute("info");
+		System.out.println(info_post.getUser_email());
+
 
 		// 파일 경로
 		String savePath = "imges";
@@ -39,11 +45,12 @@ public class PostWriteService extends HttpServlet {
 				new DefaultFileRenamePolicy());
 
 		String board_type = multi.getParameter("board_type");
+		String user_email = info_post.getUser_email();
 		String board_title = multi.getParameter("board_title");
 		String board_content = multi.getParameter("board_content");
 		String board_pic = multi.getFilesystemName("board_pic");
 
-		BoardsVO vo = new BoardsVO(board_type, board_title, board_content, board_pic);
+		BoardsVO vo = new BoardsVO(board_type, user_email, board_title, board_content, board_pic);
 
 		BoardsDAO dao = new BoardsDAO();
 		int res = dao.postWrite(vo);
